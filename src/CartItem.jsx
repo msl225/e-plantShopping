bd5de6703b2f45d783e58f1bbd5473a6bc402bcf
msl225 <lumendez956@gmail.com>
2026-09-22 08:47:07 -0400

    updated CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,50 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
+    let total = 0;
+    cart.forEach((item) => {
+        const costItem = parseFloat(item.cost.substring(1));
+        total += costItem * item.quantity;
+    });
+
+    return total
  
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
+  };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({
+        name: item.name,
+        quantity: item.quantity + 1
+    }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+        dispatch(updateQuantity({
+            name: item.name,
+            quantity: item.quantity - 1
+        }));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const costItem = parsefloat(item.cost.substring(1));
+    return costItem * item.quantity;
   };
 
   return (
